e29f0954ffdf3f8ec766ad09b7970db702b6018c
Nassim Boulechfar <in_boulechfar@esi.dz>
2021-07-16 00:26:31 +0100

    add logging capabilities

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('"/" endpoint was reached')
     return "Hello World!"
 
 @app.route("/status")
 def health_check():
+    app.logger.info('"/status" endpoint was reached')
     response = app.response_class(
         response=json.dumps({"result": "OK - healty"}),
         status=200,
@@ -18,6 +21,7 @@ def health_check():
 
 @app.route("/metrics")
 def get_metrics():
+    app.logger.info('"/metrics" endpoint was reached')
     return {
         "status": "success",
         "code": 0,
@@ -28,4 +32,11 @@ def get_metrics():
     }
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename="app.log",
+        format="{levelname}:{name}: [{asctime}] {message}",
+        datefmt="%a %d %b %Y %X",
+        style="{",
+        level=logging.DEBUG
+    )
     app.run(host='0.0.0.0')
